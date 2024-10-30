5772f912d1c9395368985c017dbac85c21d382ed
Sullivan Xiong <sxiong5307@gmail.com>
2024-10-29 23:13:54 -0700

    opencv non-working library

--- ml-auction-house/electron-src/ocr/image-stream.js
+++ ml-auction-house/electron-src/ocr/image-stream.js
@@ -2,10 +2,26 @@ path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 const screenshot = require("screenshot-desktop");
 const activeWindow = require("active-win");
-const { Jimp, JimpMime } = require("jimp");
+const { Jimp } = require("jimp");
 
-function imageCoordinatesCorrection(x, y, width, height) {
-  return { x: x + 50, y: y + 35, width: width + 340, height: height + 160 };
+function bufferToDataURL(buffer) {
+  const base64 = buffer.toString("base64");
+  return `data:image/png;base64,${base64}`;
+}
+
+async function readImageFromBuffer(buffer) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => {
+      // Now img can be used with OpenCV's imread
+      const mat = cv.imread(img);
+      resolve(mat);
+    };
+    img.onerror = (err) => reject(err);
+
+    // Set the image source to the Data URL
+    img.src = bufferToDataURL(buffer);
+  });
 }
 
 async function captureScreenStream() {
@@ -13,34 +29,14 @@ async function captureScreenStream() {
 
   if (win.owner.name.includes("MapleLegends")) {
     imageCoords = win.bounds;
-    // console.log("MapleLegends window found at:", imageCoords.x, imageCoords.y, imageCoords.width, imageCoords.height);
-    // const { x, y, width, height } = imageCoordinatesCorrection(
-    //   imageCoords.x,
-    //   imageCoords.y,
-    //   imageCoords.width,
-    //   imageCoords.height
-    // );
     const { x, y, width, height } = imageCoords;
-    // console.log("MapleLegends window found at:", x, y, width, height);
 
     const imageBuffer = await screenshot();
-
-    // console.log("Image exif:", JSON.stringify(exif));
 
     const image = await Jimp.read(imageBuffer);
     const croppedImage = image.crop({ w: width, h: height, x: x, y: y });
 
-    if (process.env.ENV === "DEV") {
-      //   await croppedImage
-      //     .greyscale()
-      //     .resize({ w: croppedImage.bitmap.width * 5, h: croppedImage.bitmap.height * 5 })
-      //     .write("maplelegends.jpeg");
-    }
-
-    let resultImage = await croppedImage
-      .greyscale()
-      .resize({ w: croppedImage.bitmap.width * 1.2, h: croppedImage.bitmap.height * 1.2 })
-      .getBuffer(JimpMime.jpeg);
+    let resultImage = await croppedImage.greyscale();
     if (!resultImage || resultImage.length === 0) {
       throw new Error("Image not captured or empty");
     }
@@ -51,4 +47,4 @@ async function captureScreenStream() {
   throw new Error("MapleLegends window not found");
 }
 
-module.exports = { captureScreenStream };
+module.exports = { captureScreenStream, readImageFromBuffer };
